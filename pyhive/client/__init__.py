8b131a6b82ccf4cf62d4568578d2d3512d94909f
Michael K. Steinberg <m.kuper.steinberg@gmail.com>
2025-11-06 16:14:04 +0200

    Added Hive api version check

--- pyhive/client/__init__.py
+++ pyhive/client/__init__.py
@@ -13,6 +13,11 @@ from .queues import QueuesClientMixin
 from .subjects import SubjectClientMixin
 from .users import UserClientMixin
 from .version import VersionClientMixin
+from ..src.api_versions import (
+    SUPPORTED_API_VERSIONS,
+    LATEST_API_VERSION,
+    MIN_API_VERSION,
+)
 
 
 class HiveClient(  # pylint: disable=too-many-ancestors,abstract-method
@@ -30,6 +35,11 @@ class HiveClient(  # pylint: disable=too-many-ancestors,abstract-method
 ):
     """Aggregated HTTP client for accessing Hive API resources."""
 
+    def __init__(self, *args, skip_version_check: bool = False, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not skip_version_check:
+            self._api_version_check()
+
     def __repr__(self) -> str:
         """Return a short representation including username and hive_url.
 
@@ -45,7 +55,6 @@ class HiveClient(  # pylint: disable=too-many-ancestors,abstract-method
         lifecycle; entering the context returns the authenticated client so
         callers can perform API calls.
         """
-
         return self
 
     def __exit__(
@@ -61,3 +70,23 @@ class HiveClient(  # pylint: disable=too-many-ancestors,abstract-method
         """
 
         self._session.__exit__(type_, value, traceback)
+
+    def _api_version_check(self) -> None:
+        """Validate that the Hive server API version is supported.
+
+        Fetches the server version via ``get_hive_version`` and verifies it is present
+        in ``SUPPORTED_API_VERSIONS``. If unsupported, raises a RuntimeError with
+        guidance to align the client and server versions.
+
+        Raises:
+            RuntimeError: If the server API version is not supported by this client.
+        """
+        version_str = self.get_hive_version()
+        if version_str not in SUPPORTED_API_VERSIONS:
+            supported_range = f"{MIN_API_VERSION} .. {LATEST_API_VERSION}"
+            raise RuntimeError(
+                (
+                    f"Unsupported Hive API version '{version_str}'. Supported versions: {supported_range}. "
+                    f"Please upgrade/downgrade the server or use a compatible client."
+                )
+            )
